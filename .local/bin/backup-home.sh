216a0bf2fbffa2272f339dc3223188895a6b142b
Jasur Sadikov <jasur@sadikoff.com>
2024-04-07 22:54:30 +0200

    🕚 22:58 📆 2024-04-07

--- .local/bin/backup-home.sh
+++ .local/bin/backup-home.sh
@@ -7,7 +7,7 @@ mv .git~ .git
 if ! git status | grep -q "nothing to commit"; then
 	notify-send "Configurations updated" "Backing up your home directory"
 	git add .
-	git commit -m "$(emoiji-clock) $(date +'%H:%M 📆 %Y-%m-%d')"
+	git commit -m "$(emoji-clock) $(date +'%H:%M 📆 %Y-%m-%d')"
 	git push
 fi;
 
