f85dd91995eb3655e7c56683cf1d440512f391e7
Jasur Sadikov <jasur@sadikoff.com>
2024-08-06 09:51:38 +0200

    🕛 09:51 📆 2024-08-06

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -3,11 +3,13 @@
 # ----------------------------------
 
 alias ls='exa'
+alias ff='fastfetch'
 alias cat='bat'
 alias grep='rg'
+alias edit='micro'
 alias calc='gnome-calculator &'
-alias logout='gnome-session-quit --logout --no-prompt'
 alias xorw='echo "$WAYLAND_DISPLAY $XDG_SESSION_TYPE"'
+alias logout='gnome-session-quit --logout --no-prompt'
 
 # Git
 alias mud='~/Documents/personal/mud/mud.py'
@@ -15,5 +17,4 @@ alias wip='git add .;git commit -m "WIP";git push'
 
 alias home-enable='cd ~;mv .git~ .git; cd --'
 alias home-disable='cd ~;mv .git .git~; cd --'
-alias home-update='home-update;home-enable'
-alias home-update-once='home-update;' 
+alias home-update='home-update;' 
