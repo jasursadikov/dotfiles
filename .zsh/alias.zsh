e83617dc0df2518ecc76765eed03b9ae9c37fd4c
Jasur Sadikov <jasur@sadikoff.com>
2024-10-01 14:28:09 +0200

    🕝 14:28 📆 2024-10-01

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -9,11 +9,77 @@ alias grep='rg'
 alias edit='micro'
 alias calc='gnome-calculator &'
 alias xorw='echo "$WAYLAND_DISPLAY $XDG_SESSION_TYPE"'
-alias logout='gnome-session-quit --logout --no-prompt'
-
-# Git
 alias mud='~/Documents/personal/mud/mud.sh'
-alias wip='git add .;git commit -m "WIP";git push'
 
-alias home-enable='cd ~;mv .git~ .git; cd --'
-alias home-disable='cd ~;mv .git .git~; cd --'
+logout(){
+	gnome-session-quit --logout --no-prompt	
+}
+
+home-enable(){
+	cd ~
+	mv .git~ .git 
+	cd --
+}
+
+home-disable(){
+	cd ~
+	mv .git .git~
+	 cd --
+}
+
+home-update(){
+	cd ~
+	mv .git~ .git
+	
+	(( minutes = $(date '+%M') + 15 ))
+	(( hour = $(date '+%I') + minutes / 60 ))
+	(( minutes %= 60 )); (( hour %= 12 ))
+	
+	case $hour in
+	   0) clock="🕛"; [ $minutes -ge 30 ] && clock="🕧";;
+	   1) clock="🕐"; [ $minutes -ge 30 ] && clock="🕜";;
+	   2) clock="🕑"; [ $minutes -ge 30 ] && clock="🕝";;
+	   3) clock="🕒"; [ $minutes -ge 30 ] && clock="🕞";;
+	   4) clock="🕓"; [ $minutes -ge 30 ] && clock="🕟";;
+	   5) clock="🕔"; [ $minutes -ge 30 ] && clock="🕠";;
+	   6) clock="🕕"; [ $minutes -ge 30 ] && clock="🕡";;
+	   7) clock="🕖"; [ $minutes -ge 30 ] && clock="🕢";;
+	   8) clock="🕗"; [ $minutes -ge 30 ] && clock="🕣";;
+	   9) clock="🕘"; [ $minutes -ge 30 ] && clock="🕤";;
+	  10) clock="🕙"; [ $minutes -ge 30 ] && clock="🕥";;
+	  11) clock="🕚"; [ $minutes -ge 30 ] && clock="🕦";;
+	   *) clock="⌛";;
+	esac
+	
+	if ! git status | grep -q "nothing to commit"; then
+		notify-send "Configurations updated" "Backing up your home directory"
+		git add .
+		git commit -m "$clock $(date +'%H:%M 📆 %Y-%m-%d')"
+		git push
+	fi;
+	
+	mv .git .git~
+	
+	exit	
+}
+
+wip(){
+	git add .
+	git commit -m "WIP"
+	git push
+}
+
+copyclip() {
+    if [ -f "$1" ]; then
+		xsel --clipboard < figma.txt
+        echo "Content of \e[32m$1\e[0m is copied to the clipboard 📋"
+    else
+        echo "\e[31mFile not found:\e[0m $1"
+    fi
+}
+
+newpass() {
+	PASS_LENGTH=20
+	tr -dc 'A-Za-z0-9@#$%^&*()-_+=' </dev/urandom | head -c $PASS_LENGTH | xclip -selection clipboard	
+	echo 'New password 🔑 is created and placed in your clipboard 📋'
+}
