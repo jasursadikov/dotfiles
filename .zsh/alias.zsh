3fef82137102472e3b686386e2a276f004bcd058
Jasur Sadikov <jasur@sadikoff.com>
2024-10-02 14:58:03 +0200

    🕒 14:58 📆 2024-10-02

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -3,13 +3,15 @@ alias ..='cd ..'
 alias ls='exa --group-directories-first --icons=auto --color=auto --no-quotes --classify=auto -a'
 alias tree='exa --group-directories-first --icons=auto --color=auto --no-quotes --classify=auto --tree'
 
-alias cat='bat'
-alias grep='rg'
-alias edit='micro'
+alias cat=bat
+alias grep=rg
+alias edit=micro
 
-alias cls='clear'
+alias r=ranger
 
-alias ff='fastfetch'
+alias cls=clear
+
+alias ff=fastfetch
 
 alias calc='gnome-calculator &'
 
