5186dc30f9452923a99a7b33087d53a9579ce796
Jasur Sadikov <jasur@sadikoff.com>
2024-10-02 10:47:42 +0200

    🕚 10:47 📆 2024-10-02

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,8 +1,14 @@
-alias ls='exa --group-directories-first --icons=auto --color=auto --no-quotes --classify=auto'
+alias ..='cd ..'
+
+alias cls='clear'
 alias ff='fastfetch'
 alias cat='bat'
 alias grep='rg'
 alias edit='micro'
+
 alias calc='gnome-calculator &'
+
+alias ls='exa --group-directories-first --icons=auto --color=auto --no-quotes --classify=auto'
+
 alias xorw='echo "$WAYLAND_DISPLAY $XDG_SESSION_TYPE"'
 alias mud='~/Documents/personal/mud/mud.sh'
