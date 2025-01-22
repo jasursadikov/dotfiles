a395f2f95b823b4cc1e283bd7ce10adda66bc709
Jasur Sadikov <jasur@sadikoff.com>
2025-01-22 15:38:40 +0100

    🕞 15:38 📆 22-01-2025

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,10 +1,10 @@
-alias diff='diff --color'
-alias ls='exa -a --group-directories-first --icons=always --color=auto --no-quotes --classify=auto'
-alias lf='exa -f -a --icons=always --color=auto --no-quotes --classify=auto'
-alias tree='exa --group-directories-first --icons=auto --color=auto --no-quotes --classify=auto --tree'
 alias cat=bat
-alias py=python
 alias grep=rg
+alias ls='exa -a --group-directories-first --icons=always --color=auto --no-quotes --classify=auto --hyperlink'
+
+alias tree='exa --group-directories-first --icons=auto --color=auto --no-quotes --classify=auto --tree --hyperlink'
+alias diff='diff --color'
+alias py=python
 alias pactree='pactree --color'
 alias ip='ip -c'
 
