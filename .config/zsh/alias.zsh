abb56a75e249f8a8568695d50d84ee6c77e6e295
Jasur Sadikov <jasur@sadikoff.com>
2025-08-01 11:08:23 +0200

    🕚 11:08 📆 01-08-2025

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,13 +1,15 @@
+alias ip="ip -c"
 alias cat=bat
 alias grep=rg
-alias ls='exa -a --group-directories-first --icons=always --color=auto --no-quotes --classify=auto --hyperlink'
+alias diff="diff --color"
+alias pactree="pactree --color"
+alias ls="exa -a --group-directories-first --icons=always --color=auto --no-quotes --classify=auto --hyperlink"
+alias tree="exa --group-directories-first --icons=auto --color=auto --no-quotes --classify=auto --tree --hyperlink"
 
-alias 1='ls -1'
+alias 1="ls -1"
 
-alias diff='diff --color'
-alias edit=micro
-alias ip='ip -c'
-alias open=xdg-open
-alias pactree='pactree --color'
 alias py=python
-alias tree='exa --group-directories-first --icons=auto --color=auto --no-quotes --classify=auto --tree --hyperlink'
+alias edit=micro
+alias open=xdg-open
+
+alias unity="~/.cache/Unity/($UNITY_VERSION)/Editor"
